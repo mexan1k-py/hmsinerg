0f87388d7baeaeb6cba7c1437a8f2a59cafe71a0
Анатолий Терентьев <terentevtolik@yandex.ru>
2023-12-03 13:15:16 +0500

    hm1-2

---
+++ main.py
@@ -0,0 +1,13 @@
+#Homework 1
+vid = input('Вид питомца: ')
+age = input('Возраст: ')
+name = input('"Кличка": ')
+print(vid, name, age +' года(лет)', sep=(', '))
+
+#Homework 2
+print('Введите 4 стадии развития человека')
+stage1 = input('Первая стадия развития: ')
+stage2 = input('Вторая стадия развития: ')
+stage3 = input('Третья стадия развития: ')
+stage4 = input('Четвертая стадия развития: ')
+print(stage1, stage2, stage3, stage4, sep=(' => '))
